ed5b7d29d36d9845cbf166c599cace5f750a53fc
Bangerr <devilism09@gmail.com>
2025-03-24 19:47:14 +0100

    restructure about page and implement animations

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -9,10 +9,15 @@ import secondDishPicture from "~/assets/images/aboutPage-second.jpg";
   <NuxtLayout :overlay="'rgba(9, 13, 14, 0.90)'" :bg-image="aboutBackground">
     <main class="main-container">
       <section class="container about-section">
-        <!-- First content section: Story heading + first paragraph + first image -->
-        <div class="content-section story-section">
-          <div class="content-row">
-            <h1 class="section-heading">Our Story</h1>
+        <h1 class="heading-section">Our Story</h1>
+        <div class="story-section">
+          <div class="image-container">
+            <img
+              class="content-image"
+              src="~/assets/images/aboutPage-second.jpg"
+              alt="Bistro Bliss atmosphere" />
+          </div>
+          <div class="text-section">
             <p class="content-text">
               Founded in 1998, Bistro Bliss began as a dream to bring the warmth
               and vibrancy of Italian dining to our community. Our journey
@@ -20,24 +25,6 @@ import secondDishPicture from "~/assets/images/aboutPage-second.jpg";
               family could gather to enjoy hearty meals made with love and the
               finest ingredients.
             </p>
-            <div class="image-container">
-              <img
-                class="content-image"
-                src="~/assets/images/aboutPage-first.jpg"
-                alt="Bistro Bliss first dish" />
-            </div>
-          </div>
-        </div>
-
-        <!-- Second content section: second paragraph + second image -->
-        <div class="content-section atmosphere-section">
-          <div class="content-row">
-            <div class="image-container">
-              <img
-                class="content-image"
-                src="~/assets/images/aboutPage-second.jpg"
-                alt="Bistro Bliss atmosphere" />
-            </div>
             <p class="content-text">
               Our bistro offers a cozy and inviting atmosphere, reminiscent of
               the charming eateries found in Italy's picturesque towns. With
@@ -45,6 +32,12 @@ import secondDishPicture from "~/assets/images/aboutPage-second.jpg";
               the warmth and hospitality that are the hallmarks of Italian
               culture.
             </p>
+          </div>
+          <div class="image-container">
+            <img
+              class="content-image"
+              src="~/assets/images/aboutPage-first.jpg"
+              alt="Bistro Bliss first dish" />
           </div>
         </div>
       </section>
